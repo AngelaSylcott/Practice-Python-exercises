499e5117ac4d646227e264648468cf7babd14d01
Angela <39888269+AngelaSylcott@users.noreply.github.com>
2018-07-23 01:17:41 -0700

    Add files via upload

---
+++ ppex1.py
@@ -0,0 +1,6 @@
+# Practice Python Exercise 1: Character Input
+name = input("What is your name? " )
+print("Nice to meet you, " + name + ".")
+age = input("How old are you? " )
+print("You said you are " + age + " years old.")
+print("That means that you will turn 100 years old in", 100 - int(age), "years.")
